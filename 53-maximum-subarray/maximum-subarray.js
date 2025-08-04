413763aadb74b26e3ff85461b196666a85bcd387
Hassan Mahmood <165324384+hassan4554@users.noreply.github.com>
2025-08-04 20:10:38 +0500

    Time: 3 ms (44.35%) | Memory: 63.6 MB (58.47%) - LeetSync

---
+++ 53-maximum-subarray/maximum-subarray.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxSubArray = function (nums) {
+    let maxSum = Number.MIN_SAFE_INTEGER
+    let currentSum = 0;
+    for (let i = 0; i < nums.length; i++) {
+        currentSum += nums[i]
+        maxSum = Math.max(currentSum, maxSum)
+        if (currentSum < 0) currentSum = 0
+    }
+
+    return maxSum
+};
