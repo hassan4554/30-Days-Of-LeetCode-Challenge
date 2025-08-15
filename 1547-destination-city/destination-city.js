525f85916b0e41adac9bc0702ea83cb47c9d5853
Hassan Mahmood <165324384+hassan4554@users.noreply.github.com>
2025-08-15 22:21:56 +0500

    Time: 1 ms (87.02%) | Memory: 55.7 MB (66.41%) - LeetSync

---
+++ 1547-destination-city/destination-city.js
@@ -0,0 +1,15 @@
+/**
+ * @param {string[][]} paths
+ * @return {string}
+ */
+var destCity = function (paths) {
+    const map = new Map()
+
+    for (let i = 0; i < paths.length; i++) {
+        map.set(paths[i][0], paths[i][1])
+    }
+
+    for(let i=0;i<paths.length;i++){
+        if(!map.has(paths[i][1])) return paths[i][1]
+    }
+};
