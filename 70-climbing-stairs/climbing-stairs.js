b2c2afe97e45cd683e36ab06c282e7c397f7d211
Hassan Mahmood <165324384+hassan4554@users.noreply.github.com>
2025-08-17 23:14:03 +0500

    Time: 0 ms (100.00%) | Memory: 53.9 MB (17.47%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.js
+++ 70-climbing-stairs/climbing-stairs.js
@@ -2,21 +2,15 @@
  * @param {number} n
  * @return {number}
  */
-// const map = new Map()
+const map = new Map()
 const waysToClimb = (n, arr) => {
-    if(arr[n-1] !== -1) return arr[n-1]
-    arr[n-1]=waysToClimb(n - 1, arr) + waysToClimb(n - 2, arr)
-    return arr[n-1]
-    // if (map.has(n)) return map.get(n)
-    // map.set(n, waysToClimb(n - 1) + waysToClimb(n - 2))
-    // return map.get(n);
+    if (map.has(n)) return map.get(n)
+    map.set(n, waysToClimb(n - 1) + waysToClimb(n - 2))
+    return map.get(n);
 }
 var climbStairs = function (n) {
-    const arr = new Array(n).fill(-1)
-    arr[0]=1
-    arr[1]=2
-    // map.set(1, 1)
-    // map.set(2, 2)
-    const ways = waysToClimb(n, arr)
+    map.set(1, 1)
+    map.set(2, 2)
+    const ways = waysToClimb(n)
     return ways;
 };
