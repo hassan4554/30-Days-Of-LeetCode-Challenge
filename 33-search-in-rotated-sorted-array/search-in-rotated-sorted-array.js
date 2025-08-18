b58565bbcefdda948df8ab3e9a11574357ae0213
Hassan Mahmood <165324384+hassan4554@users.noreply.github.com>
2025-08-18 22:43:05 +0500

    Time: 0 ms (100.00%) | Memory: 54 MB (22.03%) - LeetSync

---
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function (nums, target) {
+    let start = 0;
+    let end = nums.length - 1
+
+    while (start <= end) {
+        let mid = Math.floor((start + end) / 2);
+
+        if (target === nums[mid]) return mid;
+        else if (nums[mid] >= nums[start]) {
+            if (nums[start] <= target && target <= nums[mid]) end = mid - 1;
+            else start = mid + 1
+        } else {
+            if (nums[mid] <= target && target <= nums[end]) start = mid + 1
+            else end = mid - 1
+        }
+    }
+    return -1;
+};
